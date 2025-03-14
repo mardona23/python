cf735d1cdb353507a4f26b9e9789fc95f9f0b443
mardona23 <adam_23@outlook.dk>
2025-03-14 18:08:39 +0100

    games chart.py

---
+++ chapter2/games chart.py
@@ -0,0 +1,15 @@
+#This is my skills in different games
+Brawl_stars=int(input("enter your Brawl_stars score"))
+Fortnite=int(input("enter your Fortnite score"))
+minecraft=int(input("enter your Minecraft score"))
+fifa=int(input("enter your Fifa score"))
+sum=(Brawl_stars+Fortnite+minecraft+fifa)
+per=(sum/300)*100
+print("The total percentage is",per)
+# Average of football players goals
+Ronaldo=163
+messi=753
+Neymar=376
+sum=(Ronaldo+messi+Neymar)
+average=sum/3
+print("the average of football players goal is ",average)
