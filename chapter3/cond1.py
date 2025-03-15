33061d42957d706ee0adececda31daba7759d618
mardona23 <adam_23@outlook.dk>
2025-03-15 15:26:53 +0100

    evenodd.py

---
+++ chapter3/cond1.py
@@ -0,0 +1,5 @@
+NUM=int(input("please enter a number"))
+if NUM>=0:
+    print("this is positive number")
+else:
+    print("this is negative")
---
+++ chapter3/evenodd.py
@@ -0,0 +1,6 @@
+print("Enter a number")
+n1=int(input())
+if n1%2==0:
+   print("even number")
+else: 
+    print("this is oddnumber")
