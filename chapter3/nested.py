7ce195a83519412b95e3ad79ae315d588ed99d28
mardona23 <adam_23@outlook.dk>
2025-04-12 11:08:19 +0200

    nested2.py

---
+++ chapter3/nested.py
@@ -0,0 +1,8 @@
+num=int(input("Enter your number"))
+if num>=0:
+    if num==0:
+        print("number is neutrul")
+    else:
+        print("number is positive")
+else:
+    print("number is negative")
