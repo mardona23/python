80866a2cac054fb7d0d6c174a9b5fc29db67f71c
mardona23 <adam_23@outlook.dk>
2025-04-13 11:14:02 +0200

    loop.py

---
+++ chapter3/loop.py
@@ -0,0 +1,5 @@
+for i in range(2,36,5):
+    print(i)
+a="zeeshan"
+for i in a:
+    print(i)
---
+++ chapter3/sum of numbers.py
@@ -0,0 +1,5 @@
+a=int(input("enter your number"))
+sum=0
+for i in range(1,a+1):
+    sum=sum+i
+    print(sum)
