736f72de4d3708c46a124955a2f9b1ddd2bb5328
mardona23 <adam_23@outlook.dk>
2025-05-03 18:21:07 +0200

    total-bill.py

---
+++ chapter3/Arug1.py
@@ -0,0 +1,5 @@
+def greet(name,message="hi how are you"):
+        print(message,name)
+greet("Adam")
+greet("Zeeshan")
+greet("sana")
---
+++ chapter3/totel-bill.py
@@ -0,0 +1,8 @@
+def totalbill(tb,tip):
+    total= tb + tip
+    print(f"The total bill is{total}")
+totalbill(500,260)
+#calculate the number
+def cube(number):
+    return number*number*number
+print(cube(9))
