a0c2c151e8ef2bd2737fadf803721deeecadc406
mardona23 <adam_23@outlook.dk>
2025-05-04 11:17:16 +0200

    found-the-a.py

---
+++ chapter3/braek1.py
@@ -0,0 +1,7 @@
+a=input("Enter your word")
+for i in a:
+    if i=="A":
+        print("A is found")
+        continue
+    else:
+        print("a is not found")
---
+++ chapter3/numbers.py
@@ -0,0 +1,11 @@
+def func(a,b):
+    return a+b
+x=func(6,8)
+y=func(9,7)
+print(x)
+print(y)
+# break statment
+for i in range(11):
+    if i==9:
+        continue
+    print(i)
