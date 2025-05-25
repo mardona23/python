fd1ddd05113bca639c287a008864356ab31c7437
mardona23 <adam_23@outlook.dk>
2025-05-25 11:13:20 +0200

    list1

---
+++ chapter3/data-structure/list1.py
@@ -0,0 +1,17 @@
+a1=[8,4,9,7,45]
+a2=["Adam","zeeshan"]
+print(len(a1))
+print(len(a2))
+print(a1[0])
+print(a2[0])
+print(a2[-2])
+print(a1[1:4])
+for i in a1:
+    print(i)
+a3=a1+a2
+print(a3)
+print(max(a1))
+a1.append(1)
+print(a1)
+a4=[44,65,85,31]*4
+print(a4)
