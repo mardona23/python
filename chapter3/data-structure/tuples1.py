eb380453b55ce066fcace56295abff7a9620da1c
mardona23 <adam_23@outlook.dk>
2025-05-31 11:01:00 +0200

    tuples1

---
+++ chapter3/data-structure/tuples1.py
@@ -0,0 +1,18 @@
+a=(54,83,95,57)
+print(a)
+b=("adam","zeeshan",61,2.3)
+print(b)
+print(len(a))
+print(len(b))
+for i in a:
+    print(i)
+print(a[1])
+print(a[1:4])
+tuple1=a+b
+print(tuple1)
+l1=[23,44,86,89]
+print(l1)
+l1.append(61)
+print(l1)
+print(max(l1))
+print(min(l1))
