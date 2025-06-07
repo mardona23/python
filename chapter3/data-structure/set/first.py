8bd93bc696230e0b74d17a8ff5c1e8b1087cc49d
mardona23 <adam_23@outlook.dk>
2025-06-07 10:55:01 +0200

    first

---
+++ chapter3/data-structure/set/first.py
@@ -0,0 +1,15 @@
+s1={23,61,44,54,86,44,54}
+print(s1)
+s2={2.4,54,9,79,90,90,9}
+print(s2)
+s3=s1.intersection(s2)
+print(s3)
+s4=s1.union(s2)
+print(s4)
+s1.pop()
+print(s1)
+import array
+a=array.array('d',[2.8,6.9,6.4,4.9,1.7,5.8])
+print(a)
+b=array.array('i',[2,6,4,9,7,8])
+print(b)
