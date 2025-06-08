b33e127d6b38755e432d85ca1c48c95a553e8418
mardona23 <adam_23@outlook.dk>
2025-06-08 11:13:43 +0200

    list

---
+++ chapter3/data-structure/set/listcomp.py
@@ -0,0 +1,5 @@
+a1=[1,3,6,9,12,15,18]
+a2=[num*2 for num in a1]
+print(a2)
+n1=[num for num in range(1,11)if num %2==0]
+print(n1)
---
+++ chapter3/data-structure/set/map1.py
@@ -0,0 +1,9 @@
+def power1(n):
+    return n *n
+t1=(56,4,6,48,21,19)
+r=map(power1,t1)
+print(list (r))
+name=['adam','zeeshan','bowie','ejgil']
+age=[10,44,11,11]
+a2=zip(name,age)
+print(list (a2))
